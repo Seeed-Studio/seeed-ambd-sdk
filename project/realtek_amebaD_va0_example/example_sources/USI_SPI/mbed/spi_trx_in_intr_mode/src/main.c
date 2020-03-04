2af4d8e1ef90f7d8846c9536abecb4a97cdca4a9
turmary <turmary@126.com>
2020-03-04 10:10:22 +0000

    Add: works mbed example spi_trx_in_intr_mode

--- project/realtek_amebaD_va0_example/example_sources/USI_SPI/mbed/spi_trx_in_intr_mode/src/main.c
+++ project/realtek_amebaD_va0_example/example_sources/USI_SPI/mbed/spi_trx_in_intr_mode/src/main.c
@@ -2,6 +2,7 @@
  *  Routines to access hardware
  *
  *  Copyright (c) 2014 Realtek Semiconductor Corp.
+ *  Copyright (c) 2020 Seeed Technology Co.,Ltd.
  *
  *  This module is a confidential and proprietary property of RealTek and
  *  possession or use of this module requires written permission of RealTek.
@@ -10,81 +11,39 @@
 #include "device.h"
 #include "diag.h"
 #include "main.h"
-#include "spi_api.h"
-#include "spi_ex_api.h"
+#include "usi_api.h"
+#include "usi_ex_api.h"
 #include "wait_api.h"
 
 #define DataFrameSize	8
 #define dfs_mask		0xFF
 #define Mode			0
 #define SCLK_FREQ		100000
-#define TEST_BUF_SIZE	512
+#define TEST_BUF_SIZE	256
 
-/*SPIx pin location:
+/*USI_SPI pin location:
 
-SPI0:
-
-	S0: PB_18  (MOSI)
-		PB_19  (MISO)
-		PB_20  (SCLK)
-		PB_21  (CS)
-
-
-	S1: PA_16  (MOSI)
-		PA_17  (MISO)
-		PA_18  (SCLK)
-		PA_19  (CS)
-
-SPI1:
-
-	S0: PA_12  (MOSI)
-		PA_13  (MISO)
-		PA_14  (SCLK)
-		PA_15  (CS)
-
-	S1: PB_4  (MOSI)
-		PB_5  (MISO)
-		PB_6  (SCLK)
-		PB_7  (CS)
+		PA_25  (MOSI)
+		PA_26  (MISO)
+		PA_30  (SCLK)
+		PA_28  (CS)
 */
-// SPI0 (S1)
-#define SPI0_MOSI  PA_16
-#define SPI0_MISO  PA_17
-#define SPI0_SCLK  PA_18
-#define SPI0_CS    PA_19
 
-// SPI1 (S1)
-#define SPI1_MOSI  PB_4
-#define SPI1_MISO  PB_5
-#define SPI1_SCLK  PB_6
-#define SPI1_CS    PB_7
+#define USI_SPI_MOSI	PA_25
+#define USI_SPI_MISO	PA_26
+#define USI_SPI_SCLK	PA_30
+#define USI_SPI_CS	PA_28
 
-SRAM_NOCACHE_DATA_SECTION u16 MasterTxBuf[TEST_BUF_SIZE];
-SRAM_NOCACHE_DATA_SECTION u16 MasterRxBuf[TEST_BUF_SIZE];
 SRAM_NOCACHE_DATA_SECTION u16 SlaveTxBuf[TEST_BUF_SIZE];
 SRAM_NOCACHE_DATA_SECTION u16 SlaveRxBuf[TEST_BUF_SIZE];
 
-volatile int MasterTxDone;
-volatile int MasterRxDone;
 volatile int SlaveTxDone;
 volatile int SlaveRxDone;
 
-void Master_tr_done_callback(void *pdata, SpiIrq event)
-{
-	switch(event){
-		case SpiRxIrq:
-			MasterRxDone = 1;
-			break;
-		case SpiTxIrq:
-			MasterTxDone = 1;
-			break;
-		default:
-			DBG_8195A("unknown interrput evnent!\n");
-	}
-}
-
 void Slave_tr_done_callback(void *pdata, SpiIrq event)
 {
+	(void) pdata;
+
 	switch(event){
 		case SpiRxIrq:
 			SlaveRxDone = 1;
@@ -97,7 +56,7 @@ void Slave_tr_done_callback(void *pdata, SpiIrq event)
 	}
 }
 
-BOOL SsiDataCompare(u16 *pSrc, u16 *pDst, u32 Length)
+BOOL SsiDataCompare(u16 *pSrc, u16 *pDst, int Length)
 {
 	int Index;
 	u8 *PSrc_8 = (u8*)pSrc;
@@ -106,15 +65,15 @@ BOOL SsiDataCompare(u16 *pSrc, u16 *pDst, u32 Length)
 	
 	if(DataFrameSize > 8) {
 		for (Index = 0; Index < Length; ++Index){
-			if ((pSrc[Index] & dfs_mask)!= pDst[Index]) {
-				DBG_8195A("%x: %x ---- %x\n",Index, pSrc[Index] & dfs_mask, pDst[Index]);
+			if ((pSrc[Index] & dfs_mask) != pDst[Index]) {
+				DBG_8195A("%x:   %X ---- %X\n",Index, pSrc[Index] & dfs_mask, pDst[Index]);
 				res = _FALSE;
 			}
 		}
 	} else {
 		for (Index = 0; Index < Length; ++Index){
-			if((PSrc_8[Index] & dfs_mask)!= PDst_8[Index]) {
-				DBG_8195A("%x: %x ---- %x\n",Index, PSrc_8[Index] & dfs_mask, PDst_8[Index]);
+			if((PSrc_8[Index] & dfs_mask) != PDst_8[Index]) {
+				DBG_8195A("%x:   %X ---- %X\n",Index, PSrc_8[Index] & dfs_mask, PDst_8[Index]);
 				res = _FALSE;
 			}
 		}
@@ -122,17 +81,16 @@ BOOL SsiDataCompare(u16 *pSrc, u16 *pDst, u32 Length)
 	return res;
 }
 
-void SsiPrint(u16 *pSrc, u16 *pDst, u32 Length)
+void SsiPrint(u16 *pSrc, int Length)
 {
 	int Index;
 	u8 *PSrc_8 = (u8*)pSrc;
-	u8 *PDst_8 = (u8*)pDst;
 
-	for(Index = 0;Index < Length; Index++){
+	for (Index = 0;Index < Length; Index++){
 		if(DataFrameSize > 8)
-			DBG_8195A("%x: %x ---- %x\n",Index, pSrc[Index] & dfs_mask, pDst[Index]);
+			DBG_8195A("%X: %X\n",Index, pSrc[Index] & dfs_mask);
 		else
-			DBG_8195A("%x: %x ---- %x\n",Index, PSrc_8[Index] & dfs_mask, PDst_8[Index]);
+			DBG_8195A("%X: %X\n",Index, PSrc_8[Index] & dfs_mask);
 	}
 } 
 
@@ -141,114 +99,92 @@ void SsiPrint(u16 *pSrc, u16 *pDst, u32 Length)
   * @param  None 
   * @retval None
   */
-spi_t spi_master;
 spi_t spi_slave;
 
-void main(void)
+int main(void)
 {
 
-	/* SPI1 is as Master */
-	spi_master.spi_idx=MBED_SPI1;
-	spi_init(&spi_master, SPI1_MOSI, SPI1_MISO, SPI1_SCLK, SPI1_CS);
-	spi_format(&spi_master, DataFrameSize, Mode, 0);
-	spi_frequency(&spi_master, SCLK_FREQ);
-	/* SPI0 is as Slave */
-	spi_slave.spi_idx=MBED_SPI0;
-	spi_init(&spi_slave,  SPI0_MOSI, SPI0_MISO, SPI0_SCLK, SPI0_CS);
-	spi_format(&spi_slave, DataFrameSize, Mode, 1);
+	/* USI0_DEV is as Slave */
+	spi_slave.spi_idx = MBED_USI0_SPI;
+	uspi_init(&spi_slave, USI_SPI_MOSI, USI_SPI_MISO, USI_SPI_SCLK, USI_SPI_CS);
+	uspi_format(&spi_slave, DataFrameSize, Mode, 1);
 
 	int i=0;
 	int result1 = 1;
-	int result2 = 1;
-	int result3 = 1;
 	u32 trans_bytes = (DataFrameSize > 8) ? (TEST_BUF_SIZE * 2) : TEST_BUF_SIZE;
 
-	_memset(MasterTxBuf, 0, TEST_BUF_SIZE);
-	_memset(MasterRxBuf, 0, TEST_BUF_SIZE);
-	_memset(SlaveTxBuf, 0, TEST_BUF_SIZE);
-	_memset(SlaveRxBuf, 0, TEST_BUF_SIZE);
+	LOG_MASK(LEVEL_ERROR, -1UL);
+
+	int k;
+
+	for (k = 0; k < 2; k++) {
+
+		_memset(SlaveTxBuf, 0, TEST_BUF_SIZE);
+		_memset(SlaveRxBuf, 0, TEST_BUF_SIZE);
 	
-	for(i = 0; i < TEST_BUF_SIZE;i++) {
-		if (DataFrameSize > 8)
-			*((u16*)MasterTxBuf + i) = i;
-		else
-			*((u8*)MasterTxBuf + i) = i;
-		}
-	
-	for(i = 0; i < TEST_BUF_SIZE;i++) {
-		if (DataFrameSize > 8)
-			*((u16*)SlaveTxBuf + i) = i;
-		else
-			*((u8*)SlaveTxBuf + i) = i;
+		for (i = 0; i < TEST_BUF_SIZE;i++) {
+			if (DataFrameSize > 8)
+				*((u16*)SlaveTxBuf + i) = i;
+			else
+				*((u8*)SlaveTxBuf + i) = i;
 		}
 
 
 	/**
-	* Master write/read, Slave read/write
+	* Slave read/write
 	*/
-	DBG_8195A("------Master write/read, Slave read/write-------\n");
+	DBG_8195A("------USI_SPI Slave read/write-------\n");
 	
-		spi_irq_hook(&spi_master,(spi_irq_handler) Master_tr_done_callback, (uint32_t)&spi_master);
-		spi_irq_hook(&spi_slave,(spi_irq_handler) Slave_tr_done_callback, (uint32_t)&spi_slave);
+		uspi_irq_hook(&spi_slave, (spi_irq_handler)Slave_tr_done_callback, (uint32_t)&spi_slave);
 		
-		MasterRxDone = 0;
 		SlaveRxDone = 0;
 
-		spi_slave_read_stream(&spi_slave, SlaveRxBuf, trans_bytes);
-		spi_slave_write_stream(&spi_slave, SlaveTxBuf, trans_bytes);
-		spi_master_write_read_stream(&spi_master, MasterTxBuf, MasterRxBuf, trans_bytes);
+		uspi_slave_read_stream(&spi_slave, (char*)SlaveRxBuf, trans_bytes);
+		uspi_slave_write_stream(&spi_slave, (char*)SlaveTxBuf, trans_bytes);
 		
 
 		i=0;
-		while((MasterRxDone && SlaveRxDone) == 0) {
+		while (SlaveRxDone == 0) {
 			wait_ms(100);
 			i++;
 			if (i>150) {
-				DBG_8195A("SPI Wait Timeout\r\n");
+				DBG_8195A("SPI Slave TRx Wait Timeout\r\n");
 				break;
 			}
 		}
 		
-		SsiPrint(MasterTxBuf, SlaveRxBuf, TEST_BUF_SIZE);
-		result1 = SsiDataCompare(MasterTxBuf, SlaveRxBuf, TEST_BUF_SIZE);
-		SsiPrint(SlaveTxBuf, MasterRxBuf, TEST_BUF_SIZE);
-		result2 = SsiDataCompare(SlaveTxBuf, MasterRxBuf, TEST_BUF_SIZE);
+		// SsiPrint(SlaveRxBuf, TEST_BUF_SIZE);
+		result1 = SsiDataCompare(SlaveTxBuf, SlaveRxBuf, TEST_BUF_SIZE);
 
 	/**
-	* Master read, Slave write
+	* Slave write
 	*/
-	DBG_8195A("-----------Master read, Slave write------------\n");
-	
-		spi_irq_hook(&spi_master,(spi_irq_handler) Master_tr_done_callback, (uint32_t)&spi_master);
+	DBG_8195A("-----------Slave write------------\n");
 
-		MasterRxDone = 0;
+		SlaveTxDone = 0;
 
-		spi_flush_rx_fifo(&spi_master);
-
-		spi_slave_write_stream(&spi_slave, SlaveTxBuf, trans_bytes);
-		spi_master_read_stream(&spi_master, MasterRxBuf, trans_bytes);
+		uspi_slave_write_stream(&spi_slave, (char*)SlaveTxBuf, trans_bytes);
 
 		i=0;
-		while(MasterRxDone == 0) {
+		while(SlaveTxDone == 0) {
 			wait_ms(100);
 			i++;
 			if (i>150) {
-				DBG_8195A("SPI Master Wait Timeout\r\n");
+				DBG_8195A("SPI Slave Tx Wait Timeout\r\n");
 				break;
 			}
 		}
+		uspi_flush_rx_fifo(&spi_slave);
 
-		SsiPrint(SlaveTxBuf, MasterRxBuf, TEST_BUF_SIZE);
-		result3 = SsiDataCompare(SlaveTxBuf, MasterRxBuf, TEST_BUF_SIZE);
 
-	DBG_8195A("\r\nResult is %s\r\n", (result1 && result2 && result3) ? "success" : "fail");
+		DBG_8195A("\r\nResult is %s\r\n", result1? "success" : "fail");
+	}
 	
-	spi_free(&spi_master);
-	spi_free(&spi_slave);
+	uspi_free(&spi_slave);
 
-	DBG_8195A("SPI Demo finished.\n");
+	DBG_8195A("USI SPI MBED Demo finished.\n");
 
 	for(;;);
-
+	return 0;
 }
 
