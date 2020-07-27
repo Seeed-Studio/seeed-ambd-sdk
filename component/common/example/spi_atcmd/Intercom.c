5cbeba2425fc27750e8afa7cfa0015d87cbad6d7
Takashi Matsuoka <matsujirushi@live.jp>
2020-07-27 10:34:24 +0900

    Add digital out for debug

--- component/common/example/spi_atcmd/Intercom.c
+++ component/common/example/spi_atcmd/Intercom.c
@@ -3,6 +3,7 @@
 #include <osdep_service.h>
 #include <device.h>
 #include "rtl8721d_usi_ssi.h"
+#include "DebugDout.h"
 
 #define PIN_DIR_RX			(_PA_13)
 #define PIN_EXIST_TX_DATA	(_PA_12)
@@ -42,6 +43,8 @@ static u32 USISsiInterruptHandle(void* Adaptor)
 	USI_SSI_SetIsrClean(usi_ssi_adapter->usi_dev, InterruptStatus);
 
 	if (InterruptStatus & USI_RXFIFO_ALMOST_FULL_INTS) {
+		DebugDout1(true);
+
 		u32 TransLen = USI_SSI_ReceiveData(usi_ssi_adapter->usi_dev, usi_ssi_adapter->RxData, usi_ssi_adapter->RxLength);
 		usi_ssi_adapter->RxLength -= TransLen;
 		if (usi_ssi_adapter->RxData != NULL) usi_ssi_adapter->RxData = (void*)(((u8*)usi_ssi_adapter->RxData) + TransLen);
@@ -53,6 +56,8 @@ static u32 USISsiInterruptHandle(void* Adaptor)
 			USI_SSI_INTConfig(usi_ssi_adapter->usi_dev, (USI_RXFIFO_ALMOST_FULL_INTR_EN | USI_RXFIFO_OVERFLOW_INTR_EN | USI_RXFIFO_UNDERFLOW_INTR_EN), DISABLE);
 			rtw_up_sema(&_SemaRxDone);
 		}
+
+		DebugDout1(false);
 	}
 
 	if (InterruptStatus & USI_TXFIFO_ALMOST_EMTY_INTS) {
@@ -153,6 +158,8 @@ void IntercomInit(void)
 	USISsiObj.usi_dev = USI0_DEV;
 	InterruptRegister((IRQ_FUN)USISsiInterruptHandle, USI_IRQ, (u32)&USISsiObj, 10);
 	InterruptEn(USI_IRQ, 10);
+
+	DebugDoutInit();
 }
 
 void IntercomDirRx(bool on)
@@ -167,9 +174,13 @@ void IntercomExistTxData(bool on)
 
 int IntercomRx(u8* buf, u16 len)
 {
+	DebugDout0(true);
+
 	USISsiSlaveReadStream(&USISsiObj, buf, len);
 
 	rtw_down_sema(&_SemaRxDone);
+
+	DebugDout0(false);
 
 	return len;
 }
